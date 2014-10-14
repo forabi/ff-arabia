e78ef93644367a4785e3d7c44c224daf9cc8426d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2014-10-14 21:27:33 +0300

    Implement embedded content previews using the Embedly API

--- data/post/post.js
+++ data/post/post.js
@@ -1,9 +1,9 @@
 console.debug("[arabion] Addon initialized.");
 
-var highlightCode = function() {
+function highlightCode(){
     
     hljs.configure({
-        tabReplace: '    '
+        tabReplace: "    "
     });
 
     console.debug("[arabion] Highlighting code...");
@@ -14,4 +14,65 @@ var highlightCode = function() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", highlightCode);
+function embedMedia() {
+    console.log("Embedding media...");
+
+    var links = document.querySelectorAll(".post_content a[href]");
+    var urls = [];
+
+    for (let el of links) {
+        // console.log(el, el.attributes, el.attributes.href);
+        let url = el.attributes.href.value;
+        urls.push(url);
+    }
+
+    console.log("Found embeddable content", urls);
+
+    if (!urls.length) return;
+
+    var reqUrl = "https://api.embed.ly/1/oembed?urls=" + urls.slice(0, 19).join(",") + "&format=json";
+
+    console.log(reqUrl);
+    var req = new XMLHttpRequest();
+    
+    req.onload = function() {
+        try {
+            // console.log("Embed OK", req.response, req.responseText);
+            if (req.response instanceof Array) {
+                req.response.forEach(function(embed, index) {
+                    // console.log(embed);
+                    var html = '';
+                    if (embed.html) {
+                        links[index].outerHTML = "<div class='embed'>" + embed.html + "</div>";
+                    } else if (embed.type === 'photo') {
+                        html = "\
+                            <img class='embed' src='" + embed.url + "'/>\
+                        ";
+                        links[index].outerHTML = "<div>" + html + "</div>";
+                    }
+                });
+            }
+        } catch (e) {
+            // @TODO: Stuff
+            console.log("Request error", e);
+            throw new Error(req);
+        }
+    }
+
+    req.onerror = function() {
+        console.log("Request error", e);
+        throw new Error(req);
+    };
+
+    req.open("GET", reqUrl);
+    req.responseType = "json";
+    req.send();
+}
+
+
+
+// window.onload = function() {
+embedMedia();
+highlightCode();
+// });
+// document.addEventListener("DOMContentLoaded", embedMedia);
